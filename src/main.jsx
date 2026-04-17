6b5d810978e7de1e016fc17bb8c5635a20d8eed1
sulaimanabuqamar <sulaiman.abuqamar@gmail.com>
2026-04-17 15:18:08 +0400

    admin fix?

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from "react-router-dom"
 import './index.css'
 import App from './App.jsx'
 import { ContentProvider } from './context/ContentContext'
@@ -8,9 +8,9 @@ import { ContentProvider } from './context/ContentContext'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ContentProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </ContentProvider>
   </StrictMode>,
 )
